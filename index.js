179006cb275900bd7f9b660e6e9af0a7f3e87b97
John Lanni <82123623+JohnLanni619@users.noreply.github.com>
2021-09-09 19:30:21 -0400

    added questions and markdown template

--- index.js
+++ index.js
@@ -1,7 +1,68 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const promptUser = () => {
+    inquirer.prompt(
+        [
+            {
+                type: 'input',
+                name: 'title',
+                message: 'What is the title of your project?'
+            },
+            {
+                type: 'input',
+                name: 'description',
+                message: 'Give a description of your project'
+            },
+            {
+                type: 'input',
+                name: 'installation',
+                message: 'Please explain installation instructions for your project'
+            },
+            {
+                type: 'input',
+                name: 'usage',
+                message: 'Please enter usage instructions for your application'
+            },
+            {
+                type: 'list',
+                name: 'license',
+                message: 'Which license would you like to add to your project?',
+                choices: [
+                    'MIT',
+                    'Apache',
+                    'Apache 2',
+                    'BSD'
+                ]
+            },
+            {
+                type: 'input',
+                name: 'contributing',
+                message: 'If you created an application or package you would like other developers to contribute to, please include details here.'
+            },
+            {
+                type: 'input',
+                name: 'tests',
+                message: 'Please include instructions on how to run the tests that are included in your application'
+            },
+            {
+                type: 'input',
+                name: 'github',
+                message: 'Please enter your GitHub username.'
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'Please enter your email address.'
+            }
+        ]
+    )
+    .then((answers) => {
+        console.log(JSON.stringify(answers));
+    });
+}
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -10,4 +71,5 @@ function writeToFile(fileName, data) {}
 function init() {}
 
 // Function call to initialize app
-init();
+// init();
+promptUser();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,18 +1,67 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (!license) {
+    return '';
+  } else {
+    return 
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (!license) {
+    return '';
+  } else {
+
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (!license) {
+    return '';
+  } else {
+    return renderLicenseBadge + renderLicenseLink;
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.title}
+
+  ## Description
+  ${data.description}
+
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ## License
+  ${renderLicenseSection}
+
+  ## Contributing
+  ${data.contributing}
+
+  ## Tests
+  ${data.tests}
+
+  ## Questions
+  [GitHub Profile](https://github.com/${data.github})
+  If you have any additional questions, you can email me at ${data.email}
 
 `;
 }
